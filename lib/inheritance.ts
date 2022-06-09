314ff9ea7f58b48c0670e49b5766ac1d5d478a41
Liam Scott-Russell <liam.scott.russell@gmail.com>
2022-06-09 17:59:54 +1200

    add additional inheritance examples

--- lib/inheritance.ts
+++ lib/inheritance.ts
@@ -9,6 +9,27 @@ import { type And, type Not } from "./conditional";
  * @template T - Maybe the "child" type.
  * @template U - Maybe the "base" type.
  * @returns - {@link True} or {@link False}
+ *
+ * @example <caption>Union types have some interesting properties.</caption>
+ *
+ * type A = { a: string };
+ * type B = { b: number };
+ * type C = A | B;
+ *
+ * // A member of a union always extends the union itself.
+ * type A_Extends_A = IsExtensionOf<A, A>; // True
+ * type A_Extends_B = IsExtensionOf<A, B>; // False
+ * type A_Extends_C = IsExtensionOf<A, C>; // True
+ *
+ * // A member of a union always extends the union itself.
+ * type B_Extends_A = IsExtensionOf<B, A>; // False
+ * type B_Extends_B = IsExtensionOf<B, B>; // True
+ * type B_Extends_C = IsExtensionOf<B, C>; // True
+ *
+ * // A union could extend a specific member of itself, or it could not, hence the return type is a union.
+ * type C_Extends_A = IsExtensionOf<C, A>; // True | False
+ * type C_Extends_B = IsExtensionOf<C, B>; // True | False
+ * type C_Extends_C = IsExtensionOf<C, C>; // True
  */
 export type IsExtensionOf<T, U> = T extends U ? True : False;
 
@@ -17,14 +38,49 @@ export type IsExtensionOf<T, U> = T extends U ? True : False;
  *
  * A "parent type" means that {@link U} extends {@link T}, and {@link T} does not extend {@link U} (i.e. strict).
  *
+ * The opposite of {@link IsChildOf}.
+ *
  * @template T - Maybe the "Parent" type.
  * @template U - Maybe the "Child" type.
  * @returns - {@link True} or {@link False}
+ *
+ * @example <caption>Union types are never parents/children of their members.</caption>
+ * type A = { a: string };
+ * type B = { b: number };
+ * type C = A | B;
+ *
+ * // A type can never be a parent or child of itself.
+ * type A_Extends_A = IsParentOf<A, A>; // False
+ * type A_Extends_B = IsParentOf<A, B>; // False
+ * type A_Extends_C = IsParentOf<A, C>; // False
+ *
+ * // A type can never be a parent or child of itself.
+ * type B_Extends_A = IsParentOf<B, A>; // False
+ * type B_Extends_B = IsParentOf<B, B>; // False
+ * type B_Extends_C = IsParentOf<B, C>; // False
+ *
+ * // A union will never be a parent or child of any of its members.
+ * type C_Extends_A = IsParentOf<C, A>; // False
+ * type C_Extends_B = IsParentOf<C, B>; // False
+ * type C_Extends_C = IsParentOf<C, C>; // False
  */
 export type IsParentOf<T, U> = And<
   IsExtensionOf<U, T>,
   Not<IsExtensionOf<T, U>>
 >;
+
+/**
+ * Returns {@link True} if {@link T} is a child type of {@link U}, otherwise returns {@link False}.
+ *
+ * A "child type" means that {@link T} extends {@link U}, and {@link U} does not extend {@link T} (i.e. strict).
+ *
+ * The opposite of {@link IsParentOf}.
+ *
+ * @template T - Maybe the "Child" type.
+ * @template U - Maybe the "Parent" type.
+ * @returns - {@link True} or {@link False}
+ */
+export type IsChildOf<T, U> = IsParentOf<U, T>;
 
 /**
  * Returns {@link OnYes} if {@link T} exactly matches {@link U}, otherwise returns {@link OnNo}.
