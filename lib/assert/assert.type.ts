a17ad36588f5569545276febd36945c9311d22bf
Liam Scott-Russell <liam.scott.russell@gmail.com>
2022-07-14 11:00:05 +1200

    add isJsonValue and isJsonStruct to assert

--- lib/assert/assert.type.ts
+++ lib/assert/assert.type.ts
@@ -1,13 +1,25 @@
-import { type Inheritance, type Boolean } from "..";
+import { type Inheritance, type Boolean, type Json } from "..";
 
 export type IsTrue<
   Test extends Boolean.True,
   OnTrue = Boolean.True,
   OnFalse = Boolean.False
-> = Inheritance.IsEqual<Test, Boolean.True, OnTrue, OnFalse>;
+  > = Inheritance.IsEqual<Test, Boolean.True, OnTrue, OnFalse>;
 
 export type IsFalse<
   Test extends Boolean.False,
   OnTrue = Boolean.True,
   OnFalse = Boolean.False
-> = Inheritance.IsEqual<Test, Boolean.False, OnTrue, OnFalse>;
+  > = Inheritance.IsEqual<Test, Boolean.False, OnTrue, OnFalse>;
+
+export type IsJsonStruct<
+  Test extends Json.Struct,
+  OnTrue = Boolean.True,
+  OnFalse = Boolean.False
+  > = Inheritance.IsEqual<Test, Json.Struct, OnTrue, OnFalse>;
+
+export type IsJsonValue<
+  Test extends Json.Value,
+  OnTrue = Boolean.True,
+  OnFalse = Boolean.False
+  > = Inheritance.IsEqual<Test, Json.Struct, OnTrue, OnFalse>;
